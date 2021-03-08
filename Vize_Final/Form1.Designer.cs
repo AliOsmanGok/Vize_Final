54ee04e804c524ae560156794fabd8f4c2507887
Ali osman GÖK <osmangok.og@gmail.com>
2021-03-08 18:06:24 +0300

    Proje dosyası ekle.

---
+++ Vize_Final/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Vize_Final
+{
+    partial class FormNot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVize = new System.Windows.Forms.Label();
+            this.labelFinal = new System.Windows.Forms.Label();
+            this.textBoxVize = new System.Windows.Forms.TextBox();
+            this.textBoxFinal = new System.Windows.Forms.TextBox();
+            this.buttonHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelVize
+            // 
+            this.labelVize.AutoSize = true;
+            this.labelVize.Location = new System.Drawing.Point(37, 28);
+            this.labelVize.Name = "labelVize";
+            this.labelVize.Size = new System.Drawing.Size(56, 13);
+            this.labelVize.TabIndex = 0;
+            this.labelVize.Text = "Vize Notu:";
+            // 
+            // labelFinal
+            // 
+            this.labelFinal.AutoSize = true;
+            this.labelFinal.Location = new System.Drawing.Point(37, 58);
+            this.labelFinal.Name = "labelFinal";
+            this.labelFinal.Size = new System.Drawing.Size(58, 13);
+            this.labelFinal.TabIndex = 1;
+            this.labelFinal.Text = "Final Notu:";
+            // 
+            // textBoxVize
+            // 
+            this.textBoxVize.Location = new System.Drawing.Point(134, 28);
+            this.textBoxVize.Name = "textBoxVize";
+            this.textBoxVize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVize.TabIndex = 2;
+            // 
+            // textBoxFinal
+            // 
+            this.textBoxFinal.Location = new System.Drawing.Point(134, 55);
+            this.textBoxFinal.Name = "textBoxFinal";
+            this.textBoxFinal.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFinal.TabIndex = 3;
+            // 
+            // buttonHesapla
+            // 
+            this.buttonHesapla.Location = new System.Drawing.Point(259, 28);
+            this.buttonHesapla.Name = "buttonHesapla";
+            this.buttonHesapla.Size = new System.Drawing.Size(100, 73);
+            this.buttonHesapla.TabIndex = 4;
+            this.buttonHesapla.Text = "IF  ile HESAPLA";
+            this.buttonHesapla.UseVisualStyleBackColor = true;
+            this.buttonHesapla.Click += new System.EventHandler(this.buttonHesapla_Click);
+            // 
+            // FormNot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 142);
+            this.Controls.Add(this.buttonHesapla);
+            this.Controls.Add(this.textBoxFinal);
+            this.Controls.Add(this.textBoxVize);
+            this.Controls.Add(this.labelFinal);
+            this.Controls.Add(this.labelVize);
+            this.Name = "FormNot";
+            this.Text = "NOT HESAPLAMA EKRANI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVize;
+        private System.Windows.Forms.Label labelFinal;
+        private System.Windows.Forms.TextBox textBoxVize;
+        private System.Windows.Forms.TextBox textBoxFinal;
+        private System.Windows.Forms.Button buttonHesapla;
+    }
+}
+
---
+++ Vize_Final/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vize_Final
+{
+    public partial class FormNot : Form
+    {
+        //bool durum = true;
+        string gk = "Geçti";
+        TextBox textBoxBut;
+        Label labelBut;
+
+        public FormNot()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonHesapla_Click(object sender, EventArgs e)
+        {
+            double vize=0, final=0, but=0, ort=0;
+            string harf="";
+            
+            vize = Convert.ToDouble(textBoxVize.Text);
+
+            //final = Convert.ToDouble(textBoxFinal.Text);
+
+            //ort = vize * 0.4 + final * 0.6;
+
+            if (gk == "Geçti")
+            //if (durum == true)
+            {
+                final = Convert.ToDouble(textBoxFinal.Text);
+                ort = vize * 0.4 + final * 0.6;
+            }
+            else
+            {
+                but = Convert.ToDouble(textBoxBut.Text);
+                ort = vize * 0.4 + but * 0.6;
+            }                               
+
+            //MessageBox.Show("Ortalamanız: " + ort.ToString());
+            
+            if ((vize <= 100 && vize >=0) && (final <= 100 && final >=0))         
+            //if (ort >= 0 && ort <= 100)
+            {
+                if (ort >= 82)
+                {
+                    harf = "AA";
+                    gk = "Geçti";
+                }
+                else if (ort >= 74)
+                {
+                    harf = "BA";
+                    gk = "Geçti";
+                }
+                else if (ort >= 65)
+                {
+                    harf = "BB";
+                    gk = "Geçti";
+                }
+                else if (ort >= 58)
+                {
+                    harf = "CB";
+                    gk = "Geçti";
+                }
+                else if (ort >= 50)
+                {
+                    harf = "CC";
+                    gk = "Geçti";
+                }
+                else if (ort >= 40)
+                {
+                    harf = "DC";
+                    gk = "Kaldı";
+                    //durum = false;
+                }
+                else if (ort >= 35)
+                {
+                    harf = "DD";
+                    gk = "Kaldı";
+                    //durum = false;
+                }
+                else if (ort >= 25)
+                {
+                    harf = "FD";
+                    gk = "Kaldı";
+                    //durum = false;
+                }
+                else 
+                {
+                    harf = "FF";
+                    gk = "Kaldı";
+                    //durum = false;
+                }             
+            MessageBox.Show(gk + " Notunuz: " + harf);
+            }
+            else
+            {
+                MessageBox.Show("HATALI DEĞER GİRDİNİZ!");
+            }
+
+            if (gk == "Kaldı")
+            //if (durum == false)
+            {
+                //Bütünleme için yeni label ve textbox oluşturulacak
+
+                labelBut = new Label();             // YENİ BİR LABEL TÜRETİLDİ
+                this.Controls.Add(labelBut);        // YENİ LABEL FORMA EKLENDİ
+
+                labelBut.Text = "Bütünleme Notu:";
+                labelBut.Left = 37;
+                labelBut.Top = 85;
+                labelBut.Width = 58;
+                labelBut.Height = 13;
+                labelBut.BackColor = Color.Blue;
+                labelBut.ForeColor = Color.Yellow;
+
+
+                textBoxBut = new TextBox();       // YENİ BİR TEXTBOX TÜRETİLDİ
+                this.Controls.Add(textBoxBut);      // YENİ TEXTBOX FORMA EKLENDİ
+
+                textBoxBut.Left = 135;
+                textBoxBut.Top = 81;
+                textBoxBut.BackColor = Color.Green;
+                textBoxBut.ForeColor = Color.White;
+                textBoxBut.RightToLeft = RightToLeft.Yes;
+
+                textBoxFinal.Visible = false;
+                labelFinal.Visible = false;
+            }
+        }
+    }
+}
